7077bd3e45da9f5a0d0c3ee9435fa62b46a05574
ahmedsgit <acc.ahmedsazzad@gmail.com>
2021-10-26 15:47:24 +0600

    deploy project on vercel

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,12 @@
 import Thumbnile from "./Thumbnile";
+import FlipMove from "react-flip-move";
 
 export default function Results({results}) {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 large-screen:flex flex-wrap justify-center">
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 large-screen:flex flex-wrap justify-center">
             {results.map((result)=>(
                 <Thumbnile key={result.id} result={result}/>
             ))}
-        </div>
+        </FlipMove>
     )
 }
--- components/Thumbnile.js
+++ components/Thumbnile.js
@@ -1,21 +1,24 @@
-import Image from "next/dist/client/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/dist/client/image";
+import { forwardRef } from "react";
 
-export default function Thumbnile({key, result}) {
+const Thumbnile = ({ result },ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-            <Image layout="responsive" src={ `${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}  alt="Movie poster" height={1080} width={1920}/>
+        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+            <Image layout="responsive" src={ `${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}` } alt="Movie poster" height={ 1080 } width={ 1920 } />
             <div className="p-2">
                 <p className="truncate max-w-md">{ result.overview }</p>
-                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in group-hover:font-bold">{result.title || result.original_name}</h2>
+                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in group-hover:font-bold">{ result.title || result.original_name }</h2>
                 <p className="flex items-center uppercase text-gray-200 text-xs opacity-0 group-hover:opacity-100 transition delay-200 duration-500 ease-in">
                     <p className="text-yellow-300">{ result.media_type && `${result.media_type}` }</p>
-                    <p className="px-1">{ " • " }{ result.release_date || `${result.first_air_date}` }{" • "}</p>
-                    <ThumbUpIcon className="h-5 text-blue-500"/>
+                    <p className="px-1">{ " • " }{ result.release_date || `${result.first_air_date}` }{ " • " }</p>
+                    <ThumbUpIcon className="h-5 text-blue-500" />
                     <p className="pl-1 text-white">{ result.vote_count }</p>
                 </p>
             </div>
         </div>
-    )
+    );
 }
+
+export default forwardRef(Thumbnile);
